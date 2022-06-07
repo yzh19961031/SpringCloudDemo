a8c4acba55dc7a1bfe84420b2fba17f5e75a8e10
yuanzhihao <742599783@qq.com>
2022-06-08 00:01:31 +0800

    多提交删除

--- server-1/target/test-classes/db/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `tbl_account` (`id`, `name`, `balance`) VALUES (1, 'yuan', 800);
-INSERT INTO `tbl_account` (`id`, `name`, `balance`) VALUES (2, 'zhi', 600);
-INSERT INTO `tbl_account` (`id`, `name`, `balance`) VALUES (3, 'yang', 750);
--- server-1/target/test-classes/db/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `tbl_account` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `balance` double NOT NULL,
-  PRIMARY KEY (`id`)
-);
